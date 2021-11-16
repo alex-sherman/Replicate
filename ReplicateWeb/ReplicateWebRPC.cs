89456b2467a55a0eae4372d63188b21ac03ca1e6
Alex Sherman <asherman1024@gmail.com>
2021-11-15 19:03:56 -0600

    Add an optional base class for RPC types to have access to HttpContext

---
+++ ReplicateWeb/ReplicateWebRPC.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Replicate.Web
+{
+    public class ReplicateWebRPC
+    {
+        [ReplicateIgnore]
+        public readonly IServiceProvider Services;
+        [ReplicateIgnore]
+        public HttpContext HttpContext => contextAccessor.HttpContext;
+        private readonly IHttpContextAccessor contextAccessor;
+
+        public ReplicateWebRPC(IServiceProvider services)
+        {
+            Services = services;
+            // If this fails, add `services.AddHttpContextAccessor();` in startup
+            contextAccessor = Services.GetRequiredService<IHttpContextAccessor>();
+        }
+    }
+}
